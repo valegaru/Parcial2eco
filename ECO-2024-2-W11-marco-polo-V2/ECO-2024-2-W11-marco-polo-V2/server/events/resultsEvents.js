68891a118402c6c333927505aeb048ac48160cd0
valegaru <valegaru2003@gmail.com>
2024-10-21 14:47:44 -0500

    clean

--- ECO-2024-2-W11-marco-polo-V2/ECO-2024-2-W11-marco-polo-V2/server/events/resultsEvents.js
+++
@@ -1,9 +0,0 @@
-// const db = require("../db")
-// const { getWinnerDataHandler } = require("../event-handlers/resultsHandlers")
-// const { assignRoles } = require("../utils/helpers")
-
-// const resultsEvents = (socket, io) => {
-//   socket.on('getWinnerData', getWinnerDataHandler(socket, db));
-// }
-
-// module.exports = { resultsEvents }
