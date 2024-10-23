c20d9c5394abe1389edd9589450751389a4b8196
valegaru <valegaru2003@gmail.com>
2024-10-23 08:22:12 -0500

    done

--- ECO-2024-2-W11-marco-polo-V2/ECO-2024-2-W11-marco-polo-V2/results-screen/screens/screen1.js
+++ ECO-2024-2-W11-marco-polo-V2/ECO-2024-2-W11-marco-polo-V2/results-screen/screens/screen1.js
@@ -5,31 +5,30 @@ export default function renderScreen1() {
 	app.innerHTML = `
     <h1>Scores</h1>
 		<div id="container"><ul id="players"></ul></div>
-
   `;
 
-	// Escuchar las puntuaciones iniciales de los jugadores
-	socket.on('initialPlayers', (data) => {
-		const { players } = data;
+	// Función para actualizar la lista de jugadores
+	const updatePlayerList = (players) => {
 		let playersList = '';
 
 		players.forEach((player, index) => {
-			playersList += `<li>${index + 1}. ${player.name} (${player.score} pts)</li>`;
+			playersList += `<li>${index + 1}. ${player.nickname} (${player.score} pts)</li>`;
 		});
 
 		document.getElementById('players').innerHTML = playersList;
-	});
+	};
 
 	// Escuchar las actualizaciones de puntuación
 	socket.on('updateScore', (data) => {
 		const { players } = data;
-		let playersList = '';
+		updatePlayerList(players); // Actualizar la lista de jugadores
+	});
 
-		players.forEach((player, index) => {
-			playersList += `<li>${index + 1}. ${player.name} (${player.score} pts)</li>`;
-		});
-
-		document.getElementById('players').innerHTML = playersList;
+	// Escuchar cuando un nuevo jugador se une y renderizar los jugadores
+	socket.on('userJoined', (db) => {
+		console.log('userJoined', db);
+		const { players } = db;
+		updatePlayerList(players); 
 	});
 
 	// Escuchar el anuncio del ganador y redirigir a la pantalla de ganador
